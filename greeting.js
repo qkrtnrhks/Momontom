f9c832634a9880833bc78257bdbe9fdba9cdac59
qkrtnrhks <72388181+qkrtnrhks@users.noreply.github.com>
2021-01-23 17:26:23 +0900

    .

--- greeting.js
+++ greeting.js
@@ -6,6 +6,7 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
       SHOWING_CN = "showing";
 
+      
 function saveName(text) {
     localStorage.setItem(USER_LS, text);
 }
